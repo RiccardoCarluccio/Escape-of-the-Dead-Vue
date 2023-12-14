248e8e99608b02c810764a00cc8a1eafe88a9472
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-12-14 18:37:41 +0100

    do DiceBar style

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,18 +1,15 @@
 <script>
   import DiceBar from '../components/DiceBar/DiceBar.vue';
-  import TestComponent from '../components/TestComponent.vue';
 
   export default {
     components: {
       DiceBar,
-      TestComponent,
     }
   }
 </script>
 
 <template>
   <DiceBar></DiceBar>
-  <TestComponent></TestComponent>
 </template>
 
 <style lang="scss" scoped>
