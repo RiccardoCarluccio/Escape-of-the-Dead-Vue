1398c7a8ddb76d8495d5cabacf646267d8e0ef6f
arciof <arciof@arciof.com>
2023-12-16 11:23:59 +0100

    HealthBar component

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,9 +1,11 @@
 <script>
   import DiceBar from '../components/DiceBar/DiceBar.vue';
+  import HealthBar from '../components/HealthBar/HealthBar.vue';
 
   export default {
     components: {
       DiceBar,
+      HealthBar
     }
   }
 </script>
@@ -11,10 +13,10 @@
 <template>
   <DiceBar></DiceBar>
   <DiceBar></DiceBar>
-
   <div>
     Out of component $diceCounter: {{  $diceCounter }}
   </div>
+  <HealthBar :min-value=0 :max-value=10 :value=3 />
 </template>
 
 <style lang="scss" scoped>
