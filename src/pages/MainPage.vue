127b53dd13f5e1b23d856842c206cf836ee973ae
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2024-01-26 23:14:21 +0100

    progress overlay

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -20,7 +20,17 @@
       <PlayZone class="interactive-layer"></PlayZone>
     </div>
 
-    <div class="credit-instructions-container"></div>
+    <div class="credits-instructions-container">
+      Chupa chups sesame snaps marzipan jelly beans muffin I love dessert. Sweet roll brownie cookie dragée powder candy bear claw lemon drops. Dragée muffin danish cake soufflé I love dessert fruitcake.
+
+      Macaroon tart chocolate lollipop ice cream cupcake. Pudding croissant caramels cake marshmallow gummi bears bear claw. Pudding cotton candy I love halvah I love brownie fruitcake powder.
+
+      Sweet roll cupcake topping cupcake jelly. I love dragée donut I love dragée lollipop tiramisu. Cookie candy halvah bear claw gummi bears chupa chups candy canes. Jelly chocolate croissant ice cream cake.
+
+      Bonbon gingerbread pie ice cream I love. Donut jelly-o sesame snaps halvah ice cream dragée brownie. Tiramisu croissant chocolate bar soufflé lollipop I love.
+
+      Macaroon dragée dragée gingerbread gummies lollipop I love icing. Donut fruitcake fruitcake toffee bear claw brownie cheesecake. Cookie I love powder tiramisu tootsie roll chupa chups sweet cake.
+    </div>
   </div>
 </template>
 
@@ -28,8 +38,13 @@
   @use "../../scss/partials/mixins" as *;
   @use "../../scss/partials/variables" as *;
 
+  .main-wrapper,
+  .main-container {
+    border: 1px solid black;
+  }
 
   .main-wrapper {
+    width: 960px;
     padding-top: 25px;
   }
 
@@ -37,6 +52,12 @@
     width: 960px;
     height: 600px;
     position: relative;
+  }
+
+  .credits-instructions-container {
+    width: 100%;
+    word-wrap: break-word;
+    padding: 20px;
   }
 
   .navbar-position,
